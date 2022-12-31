953bd00e0ac516034529609f75098c5f6cf7d87c
Tabish Mustufa <tabishm@gmail.com>
2022-12-30 22:22:46 -0800

    Move several functions to utils.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from .main import merge_excel_files
-from .main import time_series_linear_regression, time_series_constant_regression
-from .main import eer_male, eer_female
+from .utils import merge_excel_files
+from .utils import time_series_linear_regression, time_series_constant_regression
+from .utils import eer_male, eer_female
+
 from .main import load_mnd_data, load_strava_activities
--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import gzip
 
 import pandas as pd
 import numpy as np
-import sklearn.linear_model
-import sklearn.metrics
-from scipy.ndimage.interpolation import shift
 
-import pwlf
 import fitanalysis
+
+from . import utils
 
 # A list of files where I happened to record janky power data
 BAD_POWER_DATA = ['activities/3799945079.fit.gz']
@@ -17,145 +15,6 @@ BAD_POWER_DATA = ['activities/3799945079.fit.gz']
 def _get_cache_path():
     """Utility function to retrieve path to cached results"""
     return os.path.join(os.path.dirname(__file__), 'cache.hdf')
-
-def merge_excel_files(path):
-    """Loads and merges data from all Excel files in a directory
-
-    Loads all sheets of all [xls,xlsx] files in a directory and merges
-    them into a single dictionary of DataFrames. Identically-named
-    sheets from each Excel file will be concatenated in alphabetical
-    order of the Excel file names.
-
-    Arguments:
-        path: Path to directory of Excel files
-
-    Returns:
-        A dictionary of DataFrames"""
-
-    data = dict()
-    files = os.listdir(path)
-    files.sort()
-
-    for f in files:
-        (root, ext) = os.path.splitext(f)
-        if ext != '.xls' and ext != '.xlsx':
-            continue
-
-        excel = pd.read_excel(os.path.join(path, f), sheet_name=None)
-        for k in excel:
-            if k in data:
-                data[k] = pd.concat([data[k], excel[k]]).reset_index(drop=True)
-            else:
-                data[k] = excel[k]
-
-    return data
-
-def time_series_linear_regression(series, num_segments, units):
-    """Perform a piecewise linear regression on a time series
-
-    Arguments:
-        series: Pandas time series on which to perform regression
-        num_segments: Number of segments for piecewise regression
-        units: Time unit to use for calculating slope at each
-               breakpoint (e.g., 'D' means calculate rate per day)
-
-    Returns:
-        df: Dataframe (time-indexed) of breakpoints, with value and
-            slope calculated for each breakpoint
-        r2: Calculated R^2 score of model fit
-    """
-
-    # Convert time index to a uint array (in us) for scipy calculations
-    x = series.index.to_numpy().astype('datetime64[us]').astype(np.uint64)
-
-    if num_segments == 1:
-        # Do an ordinary linear regression
-        model = sklearn.linear_model.LinearRegression()
-        model.fit(x[:, np.newaxis], series.values)
-        breakpoints = np.array([x[0], x[-1]])
-        values = model.predict(breakpoints[:, np.newaxis])
-        slopes = np.array([model.coef_[0], np.NaN])
-        regression = model.predict(x[:, np.newaxis])
-
-    else:
-        # Do a multi-segment piecewise linear regression
-        piecewise = pwlf.PiecewiseLinFit(x, series.values)
-        breakpoints = piecewise.fit(num_segments)
-        values = piecewise.predict(breakpoints)
-        slopes = np.append(piecewise.calc_slopes(), [np.NaN])
-        regression = piecewise.predict(x)
-
-    # Get the conversion factor to desired rate units
-    c = np.uint64(np.timedelta64(np.timedelta64(1, units), 'us'))
-
-    # Construct a table of overall regression results
-    df = pd.DataFrame(index=breakpoints.astype('datetime64[us]'))
-    df['Value'] = values
-    df['Rate'] = slopes * c
-    r2 = sklearn.metrics.r2_score(series.values, regression)
-
-    return df, r2
-
-def time_series_constant_regression(series, num_segments):
-    """Fit a segmented constant model to a time series
-
-    Arguments:
-        series: Pandas time series on which to perform regression
-        num_segments: Number of segments for model
-
-    Returns:
-        Pandas series (time-indexed) of breakpoints, with constant
-        value fitted at each breakpoint
-    """
-
-    # Convert time index to a uint array (in us) for scipy calculations
-    x = series.index.to_numpy().astype('datetime64[us]').astype(np.uint64)
-
-    # Do the multi-segment constant regression and return the result
-    piecewise = pwlf.PiecewiseLinFit(x, series.values, degree=0)
-    breakpoints = piecewise.fit(num_segments)
-    return pd.Series(index=breakpoints.astype('datetime64[us]'),
-                     data=shift(piecewise.predict(breakpoints), -1, cval=np.NaN))
-
-def eer_male(weight, height, dob, pa=1.0):
-    """Male estimated energy requirements (per day) from MyNetDiary
-
-    Arguments:
-        weight: Pandas time series of weight measurements in pounds
-        height: Height, in inches
-        dob: Date of birth, as string or datetime64
-        pa: Activity level, 1.0 = sedentary, up to 1.45 for very active
-
-    Returns:
-        Time series of calculated EER on dates of weight measurements
-    """
-
-    # Calculate time series of age in days
-    age = (weight.index - np.datetime64(dob)).astype('timedelta64[D]') / 365.25
-
-    # Perform male EER calculation per MyNetDiary
-    # https://www.mynetdiary.com/supportArticle.do?articleId=328
-    return 662 - 9.53 * age + pa * (7.23 * weight + 13.71 * height)
-
-def eer_female(weight, height, dob, pa=1.0):
-    """Female estimated energy requirements (per day) from MyNetDiary
-
-    Arguments:
-        weight: Pandas time series of weight measurements in pounds
-        height: Height, in inches
-        dob: Date of birth, as string or datetime64
-        pa: Activity level, 1.0 = sedentary, up to 1.45 for very active
-
-    Returns:
-        Time series of calculated EER on dates of weight measurements
-    """
-
-    # Calculate time series of age in days
-    age = (weight.index - np.datetime64(dob)).astype('timedelta64[D]') / 365.25
-
-    # Perform female EER calculation per MyNetDiary
-    # https://www.mynetdiary.com/supportArticle.do?articleId=328
-    return 354 - 6.91 * age + pa * (4.25 * weight + 18.44 * height)
 
 def _process_fit_data(path, fname, cache=None):
     """Run calculations on a FIT file for load_strava_activities()"""
@@ -222,7 +81,7 @@ def load_mnd_data(path, eer_func, window):
         df_calories: A Pandas dataframe of processed calorie data"""
 
     # Load MyNetDiary data
-    mnd_data = merge_excel_files(path)
+    mnd_data = utils.merge_excel_files(path)
 
     # Construct a table of actual & smoothed weights
     df_weight = pd.DataFrame()
@@ -232,7 +91,7 @@ def load_mnd_data(path, eer_func, window):
 
     # Calculate weight loss rate by regressing over a rolling window
     df_weight['Rate'] = df_weight['Actual'].resample('D').interpolate().rolling(window, center=True).apply(
-        lambda x: time_series_linear_regression(x, 1, 'W')[0].iloc[0]['Rate'])
+        lambda x: utils.time_series_linear_regression(x, 1, 'W')[0].iloc[0]['Rate'])
 
     # Construct a table of baseline (weight maintenance), exercise and food calories
     df_calories = pd.DataFrame()
