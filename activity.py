8253121774e6b7db544d4ae202ed4fc64bd73b1e
Tabish Mustufa <tabishm@gmail.com>
2023-01-05 20:59:20 -0800

    Move FIT file processing into a separate function

--- activity.py
+++ activity.py
@@ -17,8 +17,35 @@ def get_cache_path():
     """Utility function to retrieve path to cached results"""
     return os.path.join(os.path.dirname(__file__), 'cache.hdf')
 
-def process_fit_file(fname, path, cache=None):
+def process_fit_file(file):
     """Run calculations on a single FIT file"""
+
+    # Load the FIT file for analysis
+    # Note: This is some rando code from github and is super slow
+    try:
+        fitfile = fitanalysis.Activity(file)
+    except TypeError:
+        # fitanalysis seems to puke on non-Garmin FIT files
+        return np.timedelta64(0), np.NaN
+
+    # Calculate UTC offset if available in FIT file
+    try:
+        m = next(fitfile.get_messages('activity'))
+        offset = np.timedelta64(m.get('local_timestamp').value - m.get('timestamp').value)
+    except AttributeError:
+        offset = np.timedelta64(0)
+
+    # Calculate 20-min average power (FTP) if available in FIT file
+    if fitfile.has_power:
+        p = fitfile.power.droplevel(level='block')
+        ftp = np.max(fitanalysis.util.moving_average(p, 20*60))
+    else:
+        ftp = np.NaN
+
+    return offset, ftp
+
+def process_one_activity(fname, path, cache=None):
+    """Run calculations on a single Strava activity"""
 
     # Calculate hash of file - to make sure cached results are valid
     full_path = os.path.join(path, fname)
@@ -41,35 +68,20 @@ def process_fit_file(fname, path, cache=None):
         file = full_path
 
     # Return null data if this is not a FIT file (e.g. for GPX files)
-    if ext != '.fit':
-        return hash, np.timedelta64(0), np.NaN
-
-    # Load the FIT file for analysis
-    # Note: This is some rando code from github and is super slow
-    try:
-        fitfile = fitanalysis.Activity(file)
-    except TypeError:
-        # fitanalysis seems to puke on non-Garmin FIT files
-        return hash, np.timedelta64(0), np.NaN
-
-    # Calculate UTC offset if available in FIT file
-    try:
-        m = next(fitfile.get_messages('activity'))
-        offset = np.timedelta64(m.get('local_timestamp').value - m.get('timestamp').value)
-    except AttributeError:
-        offset = np.timedelta64(0)
-
-    # Calculate 20-min average power (FTP) if available in FIT file
-    if fitfile.has_power and fname not in BAD_POWER_DATA:
-        p = fitfile.power.droplevel(level='block')
-        ftp = np.max(fitanalysis.util.moving_average(p, 20*60))
+    if ext == '.fit':
+        offset, ftp = process_fit_file(file)
     else:
+        offset = np.timedelta64(0)
+        ftp = np.NaN
+
+    # Drop FTP if in list of bad files
+    if fname in BAD_POWER_DATA:
         ftp = np.NaN
 
     return hash, offset, ftp
 
-def process_activities(path, files, recalculate):
-    """Run calculations on a set of FIT files"""
+def process_activities(path, files, recalculate=False):
+    """Run calculations on a set of Strava activities"""
 
     # Load cached calculation results if available
     try:
@@ -86,7 +98,7 @@ def process_activities(path, files, recalculate):
     # Run calculations on all FIT files, leveraging cached results
     calcs = pd.DataFrame()
     calcs['Filename'] = files
-    processor = functools.partial(process_fit_file, path=path, cache=cache)
+    processor = functools.partial(process_one_activity, path=path, cache=cache)
     with multiprocessing.Pool() as p:
         results = p.map(processor, calcs['Filename'])
     calcs['Hash'], calcs['UTC Offset'], calcs['Observed FTP'] = zip(*results)
