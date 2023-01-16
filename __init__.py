1c951c20e190f7fbf7ac64a8a43cd5f8154be40f
Tabish Mustufa <tabishm@gmail.com>
2023-01-15 17:55:11 -0800

    Cleanup of __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from .utils import merge_excel_files
-from .utils import time_series_linear_regression, time_series_constant_regression
-from .utils import eer_male, eer_female
+from .utils import time_series_linear_regression
+from .utils import time_series_constant_regression
+from .utils import eer_male
+from .utils import eer_female
 
 from .parse_fit import parse_fit
-from .parse_tcx_gpx import parse_tcx, parse_gpx
+from .parse_tcx_gpx import parse_tcx
+from .parse_tcx_gpx import parse_gpx
 from .parse_activity import ActivityParser
 
-from .main import load_mnd_data, load_strava_activities
+from .main import load_mnd_data
+from .main import load_strava_activities
